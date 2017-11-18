93759e5daca6fd0d3bebc5452519070cf2e5d624
Oded Shopen <odedia@gmail.com>
2017-11-18 23:02:36 +0200

    Login API

---
+++ gateway/src/main/java/com/odedia/springsession/rest/LoginManagementRest.java
@@ -0,0 +1,28 @@
+package com.odedia.springsession.rest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginManagementRest {
+	
+	@GetMapping(value = "/login")
+	public String login(HttpServletRequest request, HttpSession session) {
+	    /*
+	     * create new session if session is not new
+	     */
+	    if (!session.isNew()) {
+	        session.invalidate();
+	    }
+	    return "login";
+	}
+	
+	
+	@GetMapping("/logout")
+	public String logout() {
+	    return "redirect:/login";
+	}
+}
